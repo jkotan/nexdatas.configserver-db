841c200c10980406ad2bb6b0d62d7b5b321e7f61
Jan Kotanski <jankotan@gmail.com>
2018-08-01 09:47:43 +0200

    clean the code

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ def read(fname):
     """
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 #: (:obj:`str`) full release number
 release = '1.10.0'
 #: (:obj:`str`) release verion number
