449b9154a185dffcc6f9c89bbf25416248e2594c
Jan Kotanski <jankotan@gmail.com>
2020-11-12 15:45:59 +0100

    change sh to bash

--- .ci/install.sh
+++ .ci/install.sh
@@ -18,13 +18,13 @@ docker exec -it --user root ndts /bin/bash -c '$(service mysql start &) && sleep
 #    docker exec -it --user root ndts service mysql restart
 
 echo "install tango-db"
-docker exec -it --user root ndts /bin/sh -c 'apt-get -qq update; export DEBIAN_FRONTEND=noninteractive; apt-get install -y tango-db tango-common; sleep 10'
+docker exec -it --user root ndts /bin/bash -c 'apt-get -qq update; export DEBIAN_FRONTEND=noninteractive; apt-get install -y tango-db tango-common; sleep 10'
 if [ $? -ne "0" ]
 then
     exit -1
 fi
 echo "install tango servers"
-docker exec -it --user root ndts /bin/sh -c 'apt-get -qq update; export DEBIAN_FRONTEND=noninteractive;  apt-get -qq install -y  tango-starter tango-test liblog4j1.2-java'
+docker exec -it --user root ndts /bin/bash -c 'apt-get -qq update; export DEBIAN_FRONTEND=noninteractive;  apt-get -qq install -y  tango-starter tango-test liblog4j1.2-java'
 if [ $? -ne "0" ]
 then
     exit -1
@@ -36,13 +36,13 @@ docker exec -it --user root ndts service tango-starter restart
 
 if [ $2 = "2" ]; then
     echo "install python-pytango"
-    docker exec -it --user root ndts /bin/sh -c 'apt-get -qq update; export DEBIAN_FRONTEND=noninteractive; apt-get -qq install -y   python-pytango'
+    docker exec -it --user root ndts /bin/bash -c 'apt-get -qq update; export DEBIAN_FRONTEND=noninteractive; apt-get -qq install -y   python-pytango'
 else
     echo "install python3-pytango"
     if [ $1 = "ubuntu20.04" ]; then
-	docker exec -it --user root ndts /bin/sh -c 'apt-get -qq update; export DEBIAN_FRONTEND=noninteractive; apt-get -qq install -y   python3-tango'
+	docker exec -it --user root ndts /bin/bash -c 'apt-get -qq update; export DEBIAN_FRONTEND=noninteractive; apt-get -qq install -y   python3-tango'
     else
-	docker exec -it --user root ndts /bin/sh -c 'apt-get -qq update; export DEBIAN_FRONTEND=noninteractive; apt-get -qq install -y   python3-pytango'
+	docker exec -it --user root ndts /bin/bash -c 'apt-get -qq update; export DEBIAN_FRONTEND=noninteractive; apt-get -qq install -y   python3-pytango'
     fi
 fi
 if [ $? -ne "0" ]
