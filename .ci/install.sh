ece3a4e7bec966a9155b5e27926b7ab00dc730c6
Your Name <jankotan@gmail.com>
2022-05-13 11:37:56 +0200

    add ubuntu 22.04 tests

--- .ci/install.sh
+++ .ci/install.sh
@@ -1,35 +1,34 @@
 #!/usr/bin/env bash
 
-# workaround for incomatibility of default ubuntu 16.04 and tango configuration
-if [ "$1" = "ubuntu16.04" ]; then
-    docker exec  --user root ndts sed -i "s/\[mysqld\]/\[mysqld\]\nsql_mode = NO_ZERO_IN_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION/g" /etc/mysql/mysql.conf.d/mysqld.cnf
-fi
-if [ "$1" = "ubuntu20.04" ] || [ "$1" = "ubuntu20.10" ]|| [ "$1" = "ubuntu21.04" ]; then
-    docker exec  --user root ndts sed -i "s/\[mysql\]/\[mysqld\]\nsql_mode = NO_ZERO_IN_DATE,NO_ENGINE_SUBSTITUTION\ncharacter_set_server=latin1\ncollation_server=latin1_swedish_ci\n\[mysql\]/g" /etc/mysql/mysql.conf.d/mysql.cnf
-fi
 
 # workaround for a bug in debian9, i.e. starting mysql hangs
 if [ "$1" = "debian11" ]; then
     docker exec --user root ndts service mariadb restart
 else
     docker exec --user root ndts service mysql stop
-    if [ "$1" = "ubuntu20.04" ] || [ "$1" = "ubuntu20.10" ] || [ "$1" = "ubuntu21.04" ]; then
-	# docker exec --user root ndts /bin/bash -c 'mkdir -p /var/lib/mysql'
-	# docker exec --user root ndts /bin/bash -c 'chown mysql:mysql /var/lib/mysql'
+    if [ "$1" = "ubuntu22.04" ] || [ "$1" = "ubuntu20.04" ] || [ "$1" = "ubuntu20.10" ] || [ "$1" = "ubuntu21.04" ]; then
 	docker exec --user root ndts /bin/bash -c 'usermod -d /var/lib/mysql/ mysql'
     fi
-    docker exec  --user root ndts /bin/bash -c '$(service mysql start &) && sleep 30'
+    docker exec --user root ndts service mysql start
+    # docker exec  --user root ndts /bin/bash -c '$(service mysql start &) && sleep 30'
 fi
 
 echo "install tango-db"
 docker exec  --user root ndts /bin/bash -c 'apt-get -qq update; export DEBIAN_FRONTEND=noninteractive; apt-get -qq install -y tango-db tango-common; sleep 10'
-if [ "$?" -ne "0" ]; then exit -1; fi
+if [ "$?" -ne "0" ]; then exit 255; fi
+
+if [ "$1" = "ubuntu20.04" ] || [ "$1" = "ubuntu20.10" ] || [ "$1" = "ubuntu21.04" ] || [ "$1" = "ubuntu21.10" ] || [ "$1" = "ubuntu22.04" ]; then
+    # docker exec  --user tango ndts /bin/bash -c '/usr/lib/tango/DataBaseds 2 -ORBendPoint giop:tcp::10000  &'
+    docker exec  --user root ndts /bin/bash -c 'echo -e "[client]\nuser=root\npassword=rootpw" > /root/.my.cnf'
+    docker exec  --user root ndts /bin/bash -c 'echo -e "[client]\nuser=tango\nhost=127.0.0.1\npassword=rootpw" > /var/lib/tango/.my.cnf'
+fi
+docker exec  --user root ndts service tango-db restart
+
 
 echo "install tango servers"
 docker exec  --user root ndts /bin/bash -c 'apt-get -qq update; export DEBIAN_FRONTEND=noninteractive;  apt-get -qq install -y  tango-starter tango-test liblog4j1.2-java'
-if [ "$?" -ne "0" ]; then exit -1; fi
+if [ "$?" -ne "0" ]; then exit 255; fi
 
-docker exec  --user root ndts service tango-db restart
 docker exec --user root ndts service tango-starter restart
 
 if [ "$2" = "2" ]; then
@@ -43,13 +42,17 @@ else
 	docker exec  --user root ndts /bin/bash -c 'apt-get -qq update; export DEBIAN_FRONTEND=noninteractive; apt-get -qq install -y   python3-pytango'
     fi
 fi
-if [ "$?" -ne "0" ]; then exit -1; fi
+if [ "$?" -ne "0" ]; then exit 255; fi
 
 
 echo "install nxsconfigserver-db"
+docker exec --user root ndts chown -R tango:tango .
 if [ "$2" = "2" ]; then
-    docker exec  --user root ndts python setup.py -q install
+    docker exec  ndts python setup.py build
+    docker exec --user root ndts python setup.py  install
 else
-    docker exec  --user root ndts python3 setup.py -q install
+    docker exec  ndts python3 setup.py build
+    docker exec --user root ndts python3 setup.py  install
 fi
-if [ "$?" -ne "0" ]; then exit -1; fi
+
+if [ "$?" -ne "0" ]; then exit 255; fi
--- .ci/run.sh
+++ .ci/run.sh
@@ -8,5 +8,5 @@ else
 fi    
 if [ $? -ne "0" ]
 then
-    exit -1
+    exit 255
 fi
